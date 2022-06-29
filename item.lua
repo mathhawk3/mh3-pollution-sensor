ac7e08bc68bc820b93d047364784faeb7b0b0103
mathhawk3 <79771883+mathhawk3@users.noreply.github.com>
2022-06-29 12:24:00 -0700

    Required item.lua, added the item and recipe

--- item.lua
+++ item.lua
@@ -1,1 +1,8 @@
+local pollutionSensor = table.deepcopy(data.raw["item"]["constant-combinator"])
+pollutionSensor.name = "pollution-sensor"
 
+local recipe = table.deepcopy(data.raw["recipe"]["constant-combinator"])
+recipe.name = "pollution-sensor"
+recipe.result = "pollution-sensor"
+
+data:extend{pollutionSensor,recipe}
