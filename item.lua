ccc9de2e153e1567b28cac0c31b46abe61e66cda
mathhawk3 <79771883+mathhawk3@users.noreply.github.com>
2022-06-29 13:15:17 -0700

    Update item.lua

--- item.lua
+++ item.lua
@@ -1,6 +1,7 @@
 local pollutionSensor = table.deepcopy(data.raw["item"]["constant-combinator"])
 pollutionSensor.name = "pollution-sensor"
 pollutionSensor.place_result="pollution-sensor"
+pollutionSensor.order = 'z'
 
 local recipe = table.deepcopy(data.raw["recipe"]["constant-combinator"])
 recipe.name = "pollution-sensor"
