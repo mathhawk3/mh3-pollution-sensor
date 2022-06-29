51af44057bd79d4e7335836b22a4d0c61cc4a9da
mathhawk3 <79771883+mathhawk3@users.noreply.github.com>
2022-06-29 13:02:49 -0700

    Update entity.lua

--- entity.lua
+++ entity.lua
@@ -1,3 +1,5 @@
 local pollutionSensor = table.deepcopy(data.raw["constant-combinator"]["constant-combinator"])
 pollutionSensor.name = "pollution-sensor"
 pollutionSensor.minable.result = "pollution-sensor"
+
+data:extend{pollutionSensor}
